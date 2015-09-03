b833254de51597ab6601ad53307471f38d135194
Elliott Straube <estraube@atlassiansair2.office.atlassian.com>
2015-09-03 16:52:10 -0700

    First commit

---
+++ fc2.py
@@ -0,0 +1,12 @@
+x = ['a', 'b', 'a', 'a', 'c', 'c', 'q', 'q']
+
+def list_counter(list):
+	dict = {}
+	for i in list:
+		if i in dict:
+			dict[i] = dict[i] + 1
+		else:
+			dict[i] =1
+	return dict
+
+print (list_counter(x))# python-2
